707c376c0abd081d8b76081f34742ffd97f301e4
Glitch (hello-express) <none>
2019-04-13 20:46:31 +0000

    🍞✋ Checkpoint ./commands/color/tint.js:1095330/1415 ./commands/color/greyscale.js:1095330/1133 ./commands/image/frame.js:1095330/3064

---
+++ commands/color/tint.js
@@ -0,0 +1,36 @@
+
+const { drawImageWithTint } = require("discord.js-canvas");
+const { createCanvas, loadImage } = require("canvas");
+const request = require("node-superfetch");
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+// If the user is mentioned it will take the mentioned user pic or else the message author pic
+
+    let image = message.mentions.users.first() ? message.mentions.users.first().displayAvatarURL({format: 'png', size: 512}) :message.author.displayAvatarURL({format: 'png', size: 512});
+    let color = args[0];
+    if(!args[0]) return message.channel.send("Please provide me a color")
+    try {
+        const { body } = await request.get(image);
+        const data = await loadImage(body);
+        const canvas = createCanvas(data.width, data.height);
+		const ctx = canvas.getContext('2d');
+		ctx.drawImage(data, 0, 0);
+		drawImageWithTint(ctx,data,color, 0, 0, data.width, data.height);
+
+//MessageAttachement - Discord.js-12.0.0
+        const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'contrast.png');
+
+        message.reply(attachment);
+    } catch (err) {
+        message.reply("Something went wrong please try again!!");
+       console.log(err);
+    }
+}
+
+module.exports.help = {
+    name: "tint",
+		category: "Colour",
+		description: "Shows contrast colour correction"
+}
---
+++ commands/image/frame.js
@@ -0,0 +1,30 @@
+
+const {createCanvas, loadImage, registerFont} = require('canvas');
+const path = require('path');
+const request = require("node-superfetch");
+const Discord = require("discord.js");
+/* Export Function */
+module.exports.run = async (bot, message, args) => {
+let image = message.mentions.users.first() ? message.mentions.users.first().displayAvatarURL({format: 'png', size: 512}) :message.author.displayAvatarURL({format: 'png', size: 512});
+   try {
+    const base = await loadImage("https://github.com/dragonfire535/xiao/blob/master/assets/images/frame.png");
+    const {body} = await request.get(image);
+    const data = await loadImage(body);
+    const canvas = createCanvas(data.width, data.height);
+			const ctx = canvas.getContext('2d');
+			ctx.drawImage(data, 0, 0);
+			ctx.drawImage(base, 0, 0, data.width, data.height);
+        const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'frame.png');
+
+        message.reply(attachment);
+    } catch (err) {
+        message.reply("Something went wrong please try again!!");
+       console.log(err);
+    }
+}
+
+module.exports.help = {
+    name: "approved",
+		category: "Image",
+		description: "Shows approved image manipulation"
+}
